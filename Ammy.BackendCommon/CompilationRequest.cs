e26a90abb60844ea60b920c4ba7edce2642480fa
ionoy <mihhail.maslakov@gmail.com>
2017-01-25 16:37:11 +0200

    expanded IAmmyBackend and added TypeNames

--- Ammy.BackendCommon/CompilationRequest.cs
+++ Ammy.BackendCommon/CompilationRequest.cs
@@ -2,6 +2,9 @@
 {
     public class CompilationRequest
     {
+        public string ProjectDir { get; set; }
+        public string[] ReferenceAssemblyPaths { get; set; }
         public Source[] Sources { get; set; }
+        public bool NeedTypeReloading { get; set; }
     }
 }
--- Ammy.BackendCommon/IAmmyBackend.cs
+++ Ammy.BackendCommon/IAmmyBackend.cs
@@ -8,9 +8,13 @@ namespace Ammy.BackendCommon
 {
     public interface IAmmyBackend
     {
+        string[] DefaultNamespaces { get; }
+        bool NeedRuntimeUpdate { get; }
+        TypeNames TypeNames { get; }
+
         Type[] ProvideTypes();
         void CompilationFinished(CompilationResult result);
-
+        
         event EventHandler<CompilationRequest> CompilationRequested;
     }
 }
